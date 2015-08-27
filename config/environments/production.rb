d9b7a44a91e8cd26be7c7cf77d0f53b531e41022
The Great J <jeffrey410469@gmail.com>
2015-08-27 16:02:49 +0800

    add mailer config to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,22 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  # emergency mailgun doesnt send mail
+  config.action_mailer.smtp_settings = {
+    :port           => 587,
+    :address        => Rails.application.secrets.mailgun_smtp_server, 
+    :user_name      => Rails.application.secrets.mailgun_username,
+    :password       => Rails.application.secrets.mailgun_password,
+    :domain         => Rails.application.secrets.mailgun_api_base_url,
+    :authentication => :plain
+  }
+  config.action_mailer.default_url_options = {
+    :host => Rails.application.secrets.domain.sub("http://", "")
+  }
+  config.action_controller.default_url_options = {
+    :host => Rails.application.secrets.domain.sub("http://", "")
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
